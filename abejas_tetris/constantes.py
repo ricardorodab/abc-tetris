01e526d7884b43f5211ceb6a957dc7c3e8f987c5
Ricardo Abreu <ricardorodab19@gmail.com>
2019-09-23 02:34:15 -0500

    GUI tamaño

--- abejas_tetris/constantes.py
+++ abejas_tetris/constantes.py
@@ -4,17 +4,23 @@ __author__ = "José Ricardo Rodríguez Abreu"
 __email__ = "ricardo_rodab@ciencias.unam.mx"
 
 
+# Factor de escalado de la GUI:
+# Modificar si se desea hacer más grande o más chica la inferfaz:
+# El valor debe ser un entero mayor o igual a 1.
+
+ESCALADO=2
+
 # TAMAÑO DE TABLERO:
 
-TABLERO_ALTURA           = 14
-TABLERO_MARGEN_SUPERIOR  = 80
-TABLERO_MARGEN           = 4
+TABLERO_ALTURA           = 7 * ESCALADO
+TABLERO_MARGEN_SUPERIOR  = 40 * ESCALADO
+TABLERO_MARGEN           = 2 * ESCALADO
 
 # TAMAÑO DE BLOQUES:
                            
-ANCHO_BLOQUE     = 40
-ALTO_BLOQUE      = 40
-MARGEN_BLOQUE    = 2
+ANCHO_BLOQUE     = 20 * ESCALADO
+ALTO_BLOQUE      = 20 * ESCALADO
+MARGEN_BLOQUE    = 1 * ESCALADO
 
 # COLORES:
 
